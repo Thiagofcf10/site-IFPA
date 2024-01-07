6adfbd5424b975dc1f8e7c68a29198b60ff68ad0
thiago felipe <thiagofcf1@gmail.com>
2024-01-07 19:22:44 -0300

    first commit

---
+++ scripts/add-projetos.js
@@ -0,0 +1,26 @@
+const supabaseUrl = 'https://eklotjekflmuuqycnqnz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrbG90amVrZmxtdXVxeWNucW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTczMzkxNjQsImV4cCI6MjAxMjkxNTE2NH0._WkAqYcJHdpBj0S-pgCreX9KvCxClKQc233t6qHIiOU'
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey)
+
+
+constadd = document.querySelector('#add');
+const integradores = document.querySelector("#integradores");
+const nome = document.querySelector("#nome");
+const conclusao = document.querySelector("#conclusao");
+
+
+
+async function getlista(){
+
+  const {data, error} = await _supabase
+  .from('projetos')
+  .insert('*')
+  console.log(data)
+  
+
+  return data
+}
+
+
+
+
